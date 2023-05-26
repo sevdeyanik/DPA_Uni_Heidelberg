e1fc6a6cd87a6c0c938e2dca82c0de549faddbca
sevdeyanik <sevdeyanik@hotmail.com>
2023-05-26 15:57:34 +0200

    exercise2 finalized

--- DPA/Exercise2/hw3.cpp
+++ DPA/Exercise2/hw3.cpp
@@ -26,15 +26,34 @@ int main(int argc, char** argv) {
     int prefix_sum_a[n];
     MPI_Scan(a, prefix_sum_a, n, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
-    // Compute xi using the linear recurrence
-    x[0] = a[0];
-    for (int i = 1; i < n; i++) {
-        x[i] = x[i - 1] * a[i] + b[i];
+    // Compute the exclusive prefix-sums on the last process
+    if (rank == size - 1) {
+        int prefix_sum_exclusive[n];
+        prefix_sum_exclusive[0] = 0;
+        for (int i = 1; i < n; i++) {
+            prefix_sum_exclusive[i] = prefix_sum_a[i - 1];
+        }
+
+        // Compute xi using the linear recurrence
+        x[0] = a[0];
+        for (int i = 1; i < n; i++) {
+            x[i] = prefix_sum_exclusive[i] * x[i - 1] + b[i];
+        }
     }
 
-    // Print the computed terms xi
-    for (int i = 0; i < n; i++) {
-        cout << "x[" << i << "] = " << x[i] << endl;
+    // Gather the computed terms xi to process 0
+    int* all_x = nullptr;
+    if (rank == 0) {
+        all_x = new int[n * size];
+    }
+    MPI_Gather(x, n, MPI_INT, all_x, n, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Print the computed terms xi in process 0
+    if (rank == 0) {
+        for (int i = 0; i < n * size; i++) {
+            cout << "x[" << i << "] = " << all_x[i] << endl;
+        }
+        delete[] all_x;
     }
 
     MPI_Finalize();
